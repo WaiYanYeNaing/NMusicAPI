e5a8d65dde36007e0d3bf7f444cb12968ebb784e
waiyanyenaing <wai.yan.ye.naing@fusionsol.com>
2022-06-15 14:24:54 +0630

    first commit

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,23 @@ const colors = require('colors')
 const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
+const multer = require('multer')
 const port = process.env.PORT || 8000
 
 connectDB()
 
+const upload = multer({ dest: './uploads' })
+
 const app = express()
 
 app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ limit: '50mb', extended: true }))
+// app.use(upload.single())
+// app.use(express.static('public'))
 
 app.use('/api/goals', require('./routes/goalRoutes'))
+app.use('/api/nfts', upload.single('image'), require('./routes/nftRoutes'))
+app.use('/api/musics', require('./routes/musics'))
 
 app.use(errorHandler)
 
